6b588df15988b794e99176408a7037d98ad9f4d8
David Hollo <hollodavidstefan@gmail.com>
2018-11-29 11:23:32 +0200

    [merge] changes in user

--- LearnGit/src/User.java
+++ LearnGit/src/User.java
@@ -1,14 +1,32 @@
 
 public class User {
 
+	private String firstName;	
 	private String name;
-
+	private String lastName;
+	private String phone;
+	
 	public String getName() {
 		return name;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}	
 }
