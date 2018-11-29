2c673d4d9499956b77a1218c1c73b3c747cded74
David Hollo <hollodavidstefan@gmail.com>
2018-11-29 09:39:27 +0200

    [add] Continent java

---
+++ LearnGit/src/Continent.java
@@ -0,0 +1,14 @@
+
+public class Continent {
+
+	private Country country;
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+	
+}
